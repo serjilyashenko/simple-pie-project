01465c49d0b948b02f948807f01382db7a100d02
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-04-11 17:44:00 +0300

    project start

---
+++ src/components/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./app.css";
+
+export function App(): JSX.Element {
+  return (
+    <div>
+      <div className="app">
+        <h1>React Simple Pie</h1>
+      </div>
+    </div>
+  );
+}
