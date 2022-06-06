f44fd92a8187e61b238a631e05ddbf8e080eb02f
Serge Ilyashenko <age_bookend_0j@icloud.com>
2022-06-06 21:39:34 +0300

    simple-pie-project demo grid layout

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,46 +1,18 @@
 import React, { useEffect, useRef } from "react";
 
-
-import {simplePie, simpleDoughnut} from "simple-pie";
+import { simplePie, simpleDoughnut } from "simple-pie";
 // import {simplePie, simpleDoughnut} from "../packages/simple-pie/src/simple-pie"; // Don't commit this line uncommented. This is for Development purposes. Checkout README, please
 
-import {SimplePie, SimpleDoughnut} from "react-simple-pie"
+import { SimplePie, SimpleDoughnut } from "react-simple-pie";
 // import { SimplePie, SimpleDoughnut } from "../packages/react-simple-pie/src"; // Don't commit this line uncommented. This is for Development purposes. Checkout README, please
 
 import "./app.css";
 
 export function App(): JSX.Element {
-  const borderedPieRef = useRef<HTMLDivElement>(null);
-  const borderLessPieRef = useRef<HTMLDivElement>(null);
-  const borderedDoughnutRef = useRef<HTMLDivElement>(null);
-  const borderLessDoughnutRef = useRef<HTMLDivElement>(null);
-
   const doughnutSetContainer1 = useRef<HTMLElement>(null);
   const doughnutSetContainer2 = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    if (borderedPieRef.current) {
-      const svgElement = simplePie([2, 1, 1, 2]);
-      borderedPieRef.current.appendChild(svgElement);
-    }
-    if (borderLessPieRef.current) {
-      const svgElement = simplePie([2, 1, 1, 2], {
-        borderColor: "transparent",
-      });
-      borderLessPieRef.current.appendChild(svgElement);
-    }
-    if (borderedDoughnutRef.current) {
-      const svgElement = simpleDoughnut([2, 1, 1, 2], {
-        borderWidth: 3,
-      });
-      borderedDoughnutRef.current.appendChild(svgElement);
-    }
-    if (borderLessDoughnutRef.current) {
-      const svgElement = simpleDoughnut([2, 1, 1, 2], {
-        borderColor: "transparent",
-      });
-      borderLessDoughnutRef.current.appendChild(svgElement);
-    }
     Array.from({ length: 16 }, () =>
       Array.from({ length: Math.floor(Math.random() * 10 + 1) }, () =>
         Math.floor(Math.random() * 101)
@@ -51,7 +23,7 @@ export function App(): JSX.Element {
         const doughnut = document.createElement("div");
 
         valueElement.innerText = JSON.stringify(set);
-        doughnut.classList.add("simple-pie-container");
+        doughnut.classList.add("pie");
         doughnut.appendChild(valueElement);
         doughnut.appendChild(
           simpleDoughnut(set, { borderColor: "transparent" })
@@ -68,8 +40,8 @@ export function App(): JSX.Element {
         const doughnut = document.createElement("div");
 
         valueElement.innerText = String(value);
-        pie.classList.add("simple-pie-container");
-        doughnut.classList.add("simple-pie-container");
+        pie.classList.add("pie");
+        doughnut.classList.add("pie");
         pie.appendChild(simplePie([value]));
         doughnut.appendChild(simpleDoughnut([value]));
 
@@ -83,33 +55,137 @@ export function App(): JSX.Element {
 
   return (
     <main className="app">
-      <h1>React Simple Pie</h1>
-      <section>
-        <div className="simple-pie-container">
-          <SimplePie values={[1, 2, 1, 2]} />
+      <article>
+        <h2 className="title">React Simple Pie</h2>
+        <hr />
+
+        <div className="grid">
+          <section className="example-item">
+            <div className="pie">
+              <SimplePie values={[1, 2, 1, 2]} />
+            </div>
+            <pre>
+              <code>{"<SimplePie values={[1, 2, 1, 2]} />"}</code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div className="pie">
+              <SimplePie values={[1, 1, 1]} borderWidth={4} />
+            </div>
+            <pre>
+              <code>{"<SimplePie values={[1, 1, 1]} borderWidth={4} />"}</code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div className="pie">
+              <SimpleDoughnut values={[1, 2, 1, 2]} />
+            </div>
+            <pre>
+              <code>{"<SimpleDoughnut values={[1, 2, 1, 2]} />"}</code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div className="pie">
+              <SimpleDoughnut
+                values={[1, 1, 1]}
+                borderWidth={4}
+                borderColor="green"
+                innerRadius={0.3}
+              />
+            </div>
+            <pre>
+              <code>
+                {
+                  '<SimpleDoughnut\n  values={[1, 1, 1]}\n  borderWidth={4}\n  borderColor="green"\n  innerRadius={0.3}\n/>'
+                }
+              </code>
+            </pre>
+          </section>
         </div>
-        <div className="simple-pie-container">
-          <SimplePie values={[1, 1, 1]} borderWidth={4} />
+      </article>
+
+      <article>
+        <h2 className="title">Simple Pie</h2>
+        <hr />
+
+        <div className="grid">
+          <section className="example-item">
+            <div
+              className="pie"
+              ref={(pie) => pie?.appendChild(simplePie([2, 1, 1, 2]))}
+            />
+            <pre>
+              <code>{"simplePie([2, 1, 1, 2])"}</code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div
+              className="pie"
+              ref={(pie) =>
+                pie?.appendChild(
+                  simplePie([2, 1, 1, 2], {
+                    borderColor: "transparent",
+                  })
+                )
+              }
+            />
+            <pre>
+              <code>
+                {
+                  'simplePie([2, 1, 1, 2], {\n  borderColor: "transparent",\n"})'
+                }
+              </code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div
+              className="pie"
+              ref={(pie) =>
+                pie?.appendChild(
+                  simpleDoughnut([2, 1, 1, 2], {
+                    borderWidth: 3,
+                  })
+                )
+              }
+            />{" "}
+            <pre>
+              <code>
+                {'simpleDoughnut([2, 1, 1, 2], {\n  borderWidth: 3,\n"})'}
+              </code>
+            </pre>
+          </section>
+
+          <section className="example-item">
+            <div
+              className="pie"
+              ref={(pie) =>
+                pie?.appendChild(
+                  simpleDoughnut([2, 1, 1, 2], {
+                    borderColor: "transparent",
+                    inner: 0.5,
+                  })
+                )
+              }
+            />
+            <pre>
+              <code>
+                {
+                  'simplePie([2, 1, 1, 2], {\n  borderColor: "transparent",\n  inner: 0.5,\n"})'
+                }
+              </code>
+            </pre>
+          </section>
+
+          <section ref={doughnutSetContainer1}></section>
+
+          <section ref={doughnutSetContainer2}></section>
         </div>
-      </section>
-      <section>
-        <div className="simple-pie-container">
-          <SimpleDoughnut values={[1, 2, 1, 2]} />
-        </div>
-        <div className="simple-pie-container">
-          <SimpleDoughnut values={[1, 1, 1]} borderWidth={4} borderColor="green" />
-        </div>
-      </section>
-      <section>
-        <div className="simple-pie-container" ref={borderedPieRef} />
-        <div className="simple-pie-container" ref={borderLessPieRef} />
-      </section>
-      <section>
-        <div className="simple-pie-container" ref={borderedDoughnutRef} />
-        <div className="simple-pie-container" ref={borderLessDoughnutRef} />
-      </section>
-      <section ref={doughnutSetContainer1}></section>
-      <section ref={doughnutSetContainer2}></section>
+      </article>
     </main>
   );
 }
