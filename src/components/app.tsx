610501f5af6f47586f69132c92583d769eff0c10
Serge Ilyashenko <age_bookend_0j@icloud.com>
2022-06-14 16:25:36 +0300

    simple-pie-project random section

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 import { simplePie, simpleDoughnut } from "simple-pie";
 // import {simplePie, simpleDoughnut} from "../packages/simple-pie/src/simple-pie"; // Don't commit this line uncommented. This is for Development purposes. Checkout README, please
@@ -9,54 +9,14 @@ import { SimplePie, SimpleDoughnut } from "react-simple-pie";
 import "./app.css";
 
 export function App(): JSX.Element {
-  const doughnutSetContainer1 = useRef<HTMLElement>(null);
-  const doughnutSetContainer2 = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    Array.from({ length: 16 }, () =>
-      Array.from({ length: Math.floor(Math.random() * 10 + 1) }, () =>
-        Math.floor(Math.random() * 101)
-      )
-    ).forEach((set: number[]) => {
-      if (doughnutSetContainer1.current) {
-        const valueElement = document.createElement("div");
-        const doughnut = document.createElement("div");
-
-        valueElement.innerText = JSON.stringify(set);
-        doughnut.classList.add("pie");
-        doughnut.appendChild(valueElement);
-        doughnut.appendChild(
-          simpleDoughnut(set, { borderColor: "transparent" })
-        );
-
-        doughnutSetContainer1.current.appendChild(doughnut);
-      }
-    });
-    [1, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 26].forEach((value) => {
-      if (doughnutSetContainer2.current) {
-        const wrapper = document.createElement("div");
-        const valueElement = document.createElement("div");
-        const pie = document.createElement("div");
-        const doughnut = document.createElement("div");
-
-        valueElement.innerText = String(value);
-        pie.classList.add("pie");
-        doughnut.classList.add("pie");
-        pie.appendChild(simplePie([value]));
-        doughnut.appendChild(simpleDoughnut([value]));
-
-        wrapper.appendChild(valueElement);
-        wrapper.appendChild(pie);
-        wrapper.appendChild(doughnut);
-        doughnutSetContainer2.current.appendChild(wrapper);
-      }
-    });
-  });
-
   return (
     <main className="app">
       <article>
-        <h2 className="title">React Simple Pie</h2>
+        <h2 className="title">
+          React Simple Pie
+          <br />
+          ⚛️ 🥧 🍩
+        </h2>
         <hr />
 
         <div className="grid">
@@ -108,7 +68,11 @@ export function App(): JSX.Element {
       </article>
 
       <article>
-        <h2 className="title">Simple Pie</h2>
+        <h2 className="title">
+          Simple Pie
+          <br />
+          🥧 🍩
+        </h2>
         <hr />
 
         <div className="grid">
@@ -180,12 +144,58 @@ export function App(): JSX.Element {
               </code>
             </pre>
           </section>
-
-          <section ref={doughnutSetContainer1}></section>
-
-          <section ref={doughnutSetContainer2}></section>
         </div>
       </article>
+
+      <article>
+        <h2 className="title">Random Examples</h2>
+        <hr />
+
+        <div className="grid">
+          {Array.from({ length: 16 }, () =>
+            Array.from({ length: Math.floor(Math.random() * 10 + 1) }, () =>
+              Math.floor(Math.random() * 101)
+            )
+          ).map((set: number[]) => (
+            <section key={set[0] + Math.random()} className="example-item">
+              <div className="pie">
+                <SimpleDoughnut borderColor="transparent" values={set} />
+              </div>
+              <pre>
+                <code>{`<SimpleDoughnut\n  borderColor="transparent"\n  values={${JSON.stringify(
+                  set
+                )}}\n/>`}</code>
+              </pre>
+            </section>
+          ))}
+        </div>
+      </article>
+
+      {/* This article is for testing purposes */}
+      {/* The deal is if there is only one value in the data array, simple-pie draws circle or doughnut instead of sectors */}
+      {/* Before this solution it was buggy for some single values */}
+      {/* May be in future I'll write snapshot test for this. But now this section is to check if everything is fine */}
+      {/*<article>*/}
+      {/*  <h2 className="title">Single Sector Test</h2>*/}
+      {/*  <hr />*/}
+
+      {/*  <div className="grid">*/}
+      {/*    {[1, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 26].map((value) => (*/}
+      {/*      <>*/}
+      {/*        <section className="example-item">*/}
+      {/*          <div className="pie">*/}
+      {/*            <SimplePie values={[value]} />*/}
+      {/*          </div>*/}
+      {/*        </section>*/}
+      {/*        <section className="example-item">*/}
+      {/*          <div className="pie">*/}
+      {/*            <SimpleDoughnut values={[value]} />*/}
+      {/*          </div>*/}
+      {/*        </section>*/}
+      {/*      </>*/}
+      {/*    ))}*/}
+      {/*  </div>*/}
+      {/*</article>*/}
     </main>
   );
 }
