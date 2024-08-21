ad052072b36473d9a98239dcab658a00f3931693
Serge Ilyashenko <age_bookend_0j@icloud.com>
2024-08-21 20:33:00 +0200

    feat(homepage): add favicon.ico

--- apps/homepage/astro.config.mjs
+++ apps/homepage/astro.config.mjs
@@ -6,10 +6,20 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Simple Pie",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon.ico",
+            sizes: "32x32"
+          }
+        }
+      ],
       logo: {
-        light: './src/assets/logo-light.svg',
-        dark: './src/assets/logo-dark.svg',
-        replacesTitle: true,
+        light: "./src/assets/logo-light.svg",
+        dark: "./src/assets/logo-dark.svg",
+        replacesTitle: true
       },
       social: {
         github: "https://github.com/serjilyashenko/simple-pie-project"
