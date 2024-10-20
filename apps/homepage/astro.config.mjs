0a6a8bd7c8ac5c56bfa1207c8f41b8ef95297684
Sergio Ilyashenko <serjilyashenko@users.noreply.github.com>
2024-10-20 15:42:39 +0000

    feat(hompage): add sitemap and robots.txt

--- apps/homepage/astro.config.mjs
+++ apps/homepage/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://simple-pie.netlify.app/',
   integrations: [
     starlight({
       title: "Simple Pie",
