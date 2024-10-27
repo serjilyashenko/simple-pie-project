4225f3b7de3749d2e86aff09f05fcc5a6eda1970
Serge Ilyashenko <age_bookend_0j@icloud.com>
2024-10-27 19:14:54 +0100

    feat(homepage): add ya metrics

--- apps/homepage/astro.config.mjs
+++ apps/homepage/astro.config.mjs
@@ -37,7 +37,20 @@ export default defineConfig({
             rel: "apple-touch-icon",
             href: "/apple-touch-icon.png",
           }
+        },
+        // Yandex.Metrika counter
+        {
+          tag: "script",
+          attrs: {
+            type: "text/javascript",
+          },
+          content: "(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, \"script\", \"https://mc.yandex.ru/metrika/tag.js\", \"ym\"); ym(98742410, \"init\", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); "
+        },
+        {
+          tag: "noscript",
+          content: "<div><img src=\"https://mc.yandex.ru/watch/98742410\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div>"
         }
+        // /Yandex.Metrika counter
       ],
       logo: {
         light: "./src/assets/logo-light.svg",
